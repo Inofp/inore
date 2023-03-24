e85b73e3199fa9cde9f2e4159e7168c8f2c452e9
arimafp <ari_khvalko@mail.ru>
2023-03-24 03:17:54 +0300

    gl

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import PostForm from "./components/PostForm";
+// import Counter from "./components/Counter"; // поприкалываться со счётчиком
+// import PostItem from "./components/PostItem";
+import PostList from "./components/PostList";
+import './styles/App.css';
+
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {id:1, title: 'Limoona', body: 'Яна'},
+    {id:2, title: 'Dusty', body: 'Даша'},
+    {id:3, title: 'Spiritusik', body: 'Kirill'}
+  ])
+
+  const createPost = (newPost) => {
+    setPosts([...posts, newPost]);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PostForm create={ createPost } />
+      <PostList posts={posts} title="Список постов 1" />
     </div>
   );
 }
